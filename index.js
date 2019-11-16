c0c66fb81a2a9500c3a4c52348f89d936e3e90d1
Ivory Sherman <ivorysherman@Ivorys-MacBook-Pro.local>
2019-11-15 20:21:43 -0500

    JS cleanup

--- index.js
+++ index.js
@@ -2,53 +2,7 @@
     scs(window.jQuery, window, document);
     }(function($, window, document) {
 
-        // function shoppingList() {
-
-        //     $("#js-shopping-list-form").submit(function(e) {
-        //       e.preventDefault();
-
-        //         let textItem = $(this).find('input[name="shopping-list-entry"]').val();
-
-        //         if (textItem !== "") {	
-        //             $('.shopping-list').append((`<li>
-        //             <span class="shopping-item">${textItem}</span>
-        //             <div class="shopping-item-controls">
-        //                 <button class="shopping-item-toggle">
-        //                 <span class="button-label">check</span>
-        //                 </button>
-        //                 <button class="shopping-item-delete">
-        //                 <span class="button-label">delete</span>
-        //                 </button>
-        //             </div>
-        //             </li>`));
-    
-        //           $('#shopping-list-entry').val('');
-        //         } 
-
-        //     });
-
-        //     $('.shopping-list').on('click', '.shopping-item-toggle', function() {
-        //         $(this).parent().prev().toggleClass('shopping-item__checked');
-        //     });
-            
-        //     $('.shopping-list').on('click','.shopping-item-delete', function() {
-        //         $(this).closest('li').remove();
-        //     });
-        // }
-
-        // shoppingList();
-
         'use strict';
-
-        function createNode(element) {
-            return document.createElement(element);
-        }
-      
-        function append(parent, el) {
-          return parent.appendChild(el);
-        }
-      
-        const ul = document.getElementById('authors');
 
         function getDogImage(num) {
             let numParameter = num;
@@ -62,9 +16,6 @@
                     return response.json()
                 })
                 .then(data => {
-                    // Work with JSON data here
-                    //console.log(data);
-
                     let dogData = data.message;
 
                     function accessDogData() {
